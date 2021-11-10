4c4bb55f53634687a4525ec0e6276ceb364f3413
AgustinEscobarr <agustinescobaring@gmail.com>
2021-11-10 13:29:02 -0300

    cambio de ruta de sonido

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,11 @@
-let amanecerAudio= new Audio("../chuck_berry_johnny_b_goode_ZFo8-JqzSCM.mp3");
+let amanecerAudio= new Audio("../assets/sounds/chuck_berry_johnny_b_goode_ZFo8-JqzSCM.mp3");
 function amanecerReproducir(){
     amanecerAudio.play();
     amanecerAudio.volume=0.2;
 }
 setTimeout(function(){
     amanecerAudio.pause();
-},3000);
+},10000);
 addEventListener("load", amanecerReproducir);
 
 
